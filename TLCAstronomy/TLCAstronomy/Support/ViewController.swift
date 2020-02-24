ed0d276b458c06a64c63613e39fff4e5b2b78f19
Troy Christian <apostasy8@gmail.com>
2020-02-24 15:48:49 -0500

    commit groups

---
+++ TLCAstronomy/TLCAstronomy/Support/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TLCAstronomy
+//
+//  Created by Lambda_School_Loaner_219 on 2/24/20.
+//  Copyright © 2020 Lambda_School_Loaner_219. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
